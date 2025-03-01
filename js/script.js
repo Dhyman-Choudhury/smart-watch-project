440f970ea2d0cbfd34ebfde6dfc52e8fd55b0e5d
Dhyman Choudhury <dhyman2029@gmail.com>
2025-03-01 11:55:18 +0600

    solved plush minus button

--- js/script.js
+++ js/script.js
@@ -43,3 +43,14 @@ function selectWristSize(size){
     }  
 
 }
+
+const quantityElements = document.querySelectorAll('.quantity-button');
+for(let btn of quantityElements){
+    btn.addEventListener('click', function(event){
+        const amount = event.target.innerText === "+" ? 1 : -1; // 1 || -1
+        const quantityElement =document.getElementById('quantity');
+        const currentQuantity = parseInt(quantityElement.innerText); // 0
+        const newQuantity = Math.max(0, currentQuantity + amount) // (0, 1) => 1
+        quantityElement.innerText = newQuantity;
+    } )
+}
